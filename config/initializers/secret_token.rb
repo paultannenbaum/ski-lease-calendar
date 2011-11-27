bcae70078bca57a4e7de08382e1eb73b9db83f82
Paul Tannenbaum <paultannenbaum@gmail.com>
2011-11-27 10:41:41 -0800

    Project Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkiLeaseCalendar::Application.config.secret_token = '913f42a0d3d1286e877df1b8621c9280368ba149213fe0c0f01e1d8b6b824f02ea32b0c70363c632545baf000384fff31210374939e1f8d62d191f46f2a150b8'
