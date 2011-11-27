2473854600934c70c6e111a5b8c471b5e24215b4
Paul Tannenbaum <paultannenbaum@gmail.com>
2011-11-27 10:45:59 -0800

    [finishes #21492799] Added Event Calendar Plugin

---
+++ db/migrate/20111127184348_create_events.rb
@@ -0,0 +1,18 @@
+class CreateEvents < ActiveRecord::Migration
+  def self.up
+    create_table :events do |t|
+      t.string :name
+      t.datetime :start_at
+      t.datetime :end_at
+      t.boolean :all_day, :default => false
+      t.integer :friends
+      t.integer :carpool_seats
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :events
+  end
+end
