91b9675f8b9ceca4a2c3c2db211ea6466ed1c9b4
Paul Tannenbaum <paultannenbaum@gmail.com>
2011-11-29 21:09:52 -0800

    turned pipeline back on

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,9 @@ module SkiLeaseCalendar
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
+    
+    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
